7f014f8d0b62d5be2ccb9707e0870c5f5191c565
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2017-03-12 19:15:49 +0200

    created syncrhonous dispatcher

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+var _EVENT_ACTIONS = {}
+
+function dispatch(eventName, passenger) {
+  var actions = _EVENT_ACTIONS[eventName];
+  if (!actions) {
+    return;
+  }
+  var actionsArray = actions.arr;
+  for (var iterator in actionsArray) {
+    var action = actionsArray[iterator];
+    action(passenger);
+  }
+}
+
+function handle(eventName, action) {
+  var actions = _EVENT_ACTIONS[eventName];
+  if (!actions) {
+    actions = {
+      arr: [],
+      map: {}
+    };
+    _EVENT_ACTIONS[eventName] = actions
+  }
+  if (actions.map[eventName]) {
+    return
+  }
+  actions.map[eventName] = true;
+  actions.arr.push(action);
+}
+
